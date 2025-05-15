30a5503ed77592b55200fedd969a91669c8f94e8
IsilinBN <casa2pir@hotmail.fr>
2025-05-15 20:34:24 +0200

    [Fix] Adjust github workflow.

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
+  metafile: true,
   dts: true,
   sourcemap: true,
   clean: true,
