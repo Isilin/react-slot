4ef0d04b7deb53a316ada4c1329ae8e3f7e926be
IsilinBN <casa2pir@hotmail.fr>
2025-06-19 18:03:21 +0200

    [Fix] imports for vitest tests.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,13 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react({ jsxRuntime: 'automatic' })],
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './vitest.setup.ts',
+    setupFiles: ['./vitest.setup.ts'],
     reporters: ['default', 'junit'],
     outputFile: {
       junit: 'coverage/junit.xml',
